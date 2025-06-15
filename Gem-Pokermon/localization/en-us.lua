d2385c3c398f0e8f61aadac3eb0b512470f22fab
TGem7 <deeganmanzo1@gmail.com>
2025-06-15 17:19:08 -0400

    Add files via upload

---
+++ Gem-Pokermon/localization/en-us.lua
@@ -0,0 +1,21 @@
+return {
+    descriptions = {
+        Joker = {
+            j_Gem_mawile = {
+                name = "Mawile",
+                text = {
+                    "Gain {X:mult,C:white}X#2#{} for scoring {C:attention}Steel{} cards",
+                    "Then remove scoring card {C:dark_edition}Enhancements",
+                    "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive})",
+                } 
+            },
+            j_Gem_mega_mawile = {
+                name = "Mega Mawile",
+                text = {
+                    "Gain {X:mult,C:white}X#2#{} for scoring {C:attention}Steel{} cards",
+                    "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive})",
+                }
+            },
+        },
+    },
+}
---
+++ Gem-Pokermon/pokemon/Gems_1.lua
@@ -0,0 +1,112 @@
+function table.contains(table, element)
+  for _, value in pairs(table) do
+    if value == element then
+      return true
+    end
+  end
+  return false
+end
+
+-- Mawile 303
+local mawile={
+  name = "mawile", 
+  poke_custom_prefix = "Gem",
+  pos = {x = 1, y = 5},
+  config = {extra = {Xmult = 1, Xmult_mod = .5, eaten = 0}},
+  loc_vars = function(self, info_queue, card)
+    type_tooltip(self, info_queue, card)
+    info_queue[#info_queue + 1] = {set = 'Other', key = 'mega_poke'}
+    return { vars = { card.ability.extra.Xmult, card.ability.extra.Xmult_mod, card.ability.extra.eaten } }
+  end,
+  rarity = 3, 
+  cost = 8, 
+  enhancement_gate = 'm_steel',
+  stage = "Basic", 
+  ptype = "Metal",
+  atlas = "Gemdex3",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+    if context.cardarea == G.jokers and context.before and not context.blueprint then
+      local x_count = 0 
+      local enhanced = {}
+      for k, v in ipairs(context.scoring_hand) do
+          if v.config.center ~= G.P_CENTERS.c_base and not v.debuff and not v.vampired then 
+              enhanced[#enhanced+1] = v
+              v.vampired = true
+              if v.config.center == G.P_CENTERS.m_steel then
+                x_count = x_count + 1
+              end
+
+              v:set_ability(G.P_CENTERS.c_base, nil, true)
+              G.E_MANAGER:add_event(Event({
+                  func = function()
+                      v:juice_up()
+                      v.vampired = nil
+                      return true
+                  end
+              })) 
+              card.ability.extra.eaten = card.ability.extra.eaten + 1
+          end
+      end
+
+      if #enhanced > 0 then 
+          if x_count > 0 then
+            card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_mod * x_count
+          end
+      end
+    end
+    if context.cardarea == G.jokers and context.scoring_hand then
+      if context.joker_main then
+        return {
+         message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
+        colour = G.C.XMULT,
+        Xmult_mod = card.ability.extra.Xmult
+        }
+      end
+    end
+  end,
+  megas = { "mega_mawile" },
+}
+-- Mega Mawile 303
+local mega_mawile = {
+  name = "mega_mawile",
+  poke_custom_prefix = "Gem",
+  pos = { x = 7, y = 3 },
+  soul_pos =  {x = 8, y = 3 },
+  config = {extra = {Xmult = 1, Xmult_mod = .75}},
+  loc_vars = function(self, info_queue, card)
+    type_tooltip(self, info_queue, card)
+    info_queue[#info_queue + 1] = {set = 'Other', key = 'mega_poke'}
+    return { vars = { card.ability.extra.Xmult, card.ability.extra.Xmult_mod } }
+  end,
+  rarity = "poke_mega",
+  cost = 12,
+  stage = "Mega",
+  ptype = "Metal",
+  atlas = "GemMegas",
+  blueprint_compat = true,
+   calculate = function(self, card, context)
+    if context.cardarea == G.jokers and context.before and not context.blueprint then
+      for k, v in ipairs(context.scoring_hand) do
+        if v.config.center == G.P_CENTERS.m_steel and not v.debuff then
+          card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_mod
+        end
+      end
+    end
+    if context.cardarea == G.jokers and context.scoring_hand then
+      if context.joker_main then
+        return {
+         message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
+        colour = G.C.XMULT,
+        Xmult_mod = card.ability.extra.Xmult
+        }
+      end
+    end
+  end
+}
+
+list = {mawile, mega_mawile}
+
+return {name = "Gems1",
+list = list
+}
