c7b2188a25f4265cfa9ff6b5b30ea199db7c9fe8
TGem7 <deeganmanzo1@gmail.com>
2025-11-13 03:06:31 -0500

    Add files via upload

---
+++ consumables/mart-gem.lua
@@ -0,0 +1,205 @@
+local function find_leftmost_or_highlighted(key)
+  local target
+  if #G.jokers.highlighted == 0 then
+    target = SMODS.find_card(key)[1]
+  else
+    if #G.jokers.highlighted == 1
+        and G.jokers.highlighted[1].config.center.key == key
+        and not G.jokers.highlighted[1].debuff then
+      target = G.jokers.highlighted[1]
+    end
+  end
+  return target
+end
+
+local berrysweet = {
+  name = "berrysweet",
+  key = "berrysweet",
+  set = "Item",
+  pos = {x = 1, y = 0},
+  atlas = "Gem_sweets",
+  artist = "Emma",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_alcremie") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_alcremie"), "j_Gem_alcremie_berry")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+local lovesweet = {
+  name = "lovesweet",
+  key = "lovesweet",
+  set = "Item",
+  pos = {x = 0, y = 0},
+  atlas = "Gem_sweets",
+  artist = "Emma",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_alcremie") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_alcremie"), "j_Gem_alcremie_love")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+local starsweet = {
+  name = "starsweet",
+  key = "starsweet",
+  set = "Item",
+  pos = {x = 4, y = 0},
+  atlas = "Gem_sweets",
+  artist = "Emma",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_alcremie") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_alcremie"), "j_Gem_alcremie_star")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+local cloversweet = {
+  name = "cloversweet",
+  key = "cloversweet",
+  set = "Item",
+  pos = {x = 2, y = 0},
+  atlas = "Gem_sweets",
+  artist = "Emma",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_alcremie") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_alcremie"), "j_Gem_alcremie_clover")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+local flowersweet = {
+  name = "flowersweet",
+  key = "flowersweet",
+  set = "Item",
+  pos = {x = 2, y = 0},
+  atlas = "Gem_sweets",
+  artist = "Emma",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_alcremie") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_alcremie"), "j_Gem_alcremie_flower")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+local ribbonsweet = {
+  name = "ribbonsweet",
+  key = "ribbonsweet",
+  set = "Item",
+  pos = {x = 5, y = 0},
+  atlas = "Gem_sweets",
+  artist = "Emma",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_alcremie") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_alcremie"), "j_Gem_alcremie_ribbon")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+local scrollofdarkness = {
+  name = "scrollofdarkness",
+  key = "scrollofdarkness",
+  set = "Item",
+  pos = {x = 0, y = 0},
+  atlas = "Gem_placeholder",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_kubfu") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_kubfu"), "j_Gem_urshifu")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+local scrollofwaters = {
+  name = "scrollofwaters",
+  key = "scrollofwaters",
+  set = "Item",
+  pos = {x = 0, y = 0},
+  atlas = "Gem_placeholder",
+  cost = 4,
+  evo_item = true,
+  unlocked = true,
+  discovered = true,
+  can_use = function(self, card)
+    return find_leftmost_or_highlighted("j_Gem_kubfu") ~= nil
+  end,
+  use = function(self, card)
+    set_spoon_item(card)
+    poke_evolve(find_leftmost_or_highlighted("j_Gem_kubfu"), "j_Gem_urshifu_rapid")
+  end,
+  in_pool = function(self)
+    return false
+  end
+}
+
+list = {
+    berrysweet, lovesweet, starsweet, cloversweet, flowersweet, ribbonsweet, scrollofdarkness, scrollofwaters
+}
+
+
+return {
+  name = "Gem's Items",
+  list = list
+}
