fd3976636cc3ad97760f6719cf9365f997709980
TGem7 <deeganmanzo1@gmail.com>
2025-11-18 15:40:10 -0500

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -603,8 +603,8 @@ return {
             j_Gem_zeraora = {
                 name = "Zeraora",
                 text = {
-                  "{C:attention}Scored cards{} give {X:mult,C:white}X#1#{} and {C:money}$#3#{}. Increases by",
-                  "{X:mult,C:white}X#2#{} and {C:money}$#4#{} for each each other {X:lightning, C:black}Lightning{} joker you have",
+                  "{C:attention}Scored cards{} give {X:mult,C:white}X#5#{} and {C:money}$#6#{}. Increases by",
+                  "{X:mult,C:white}X#2#{} and {C:money}$#4#{} for each each {X:lightning, C:black}Lightning{} joker you have",
                   "{br:4}ERROR - CONTACT STEAK",
                   "After defeating a {C:attention}Boss Blind{}, turn your",
                   "{C:attention}leftmost{} joker to {X:lightning, C:black}Lightning{} type",
