a6a26e96196468ae6ee26e4b09b344f9f7c9400f
TGem7 <deeganmanzo1@gmail.com>
2025-11-18 13:56:20 -0500

    Update 309_Electrike.lua

--- pokemon/309_Electrike.lua
+++ pokemon/309_Electrike.lua
@@ -33,65 +33,21 @@ local zeraora = {
   eternal_compat = true,
   calculate = function(self, card, context)
     if context.individual and not context.end_of_round and context.cardarea == G.play and not context.other_card.debuff then
-        if context.other_card:get_id() == card.ability.extra.targets[i].id then
-            local earned = ease_poke_dollars(card, "manectric", card.ability.extra.money_mod + (find_other_poke_or_energy_type(card, "Lightning") * card.ability.extra.money_increase), true)
+            local plasma = (card, "zeraora", card.ability.extra.Xmult_multi + (find_other_poke_or_energy_type(card, "Lightning") * card.ability.extra.Xmult_multi_mod), true)
+            local earned = ease_poke_dollars(card, "zeraora", card.ability.extra.money_mod + (find_other_poke_or_energy_type(card, "Lightning") * card.ability.extra.money_increase), true)
             return {
                Xmult = card.ability.extra.Xmult_multi
               dollars = earned,
               card = card
             }
-        end
     end
   end,
-}
-
---  Mega Manectric 310
-local mega_manectric = {
-  name = "mega_manectric",
-  pos = {x = 4, y = 5},
-  soul_pos = {x = 5, y = 5},
-  config = {extra = {Xmult = 2, targets = {{value = "Ace", id = "14"}, {value = "King", id = "13"}, {value = "Queen", id = "12"}}}},
-  loc_vars = function(self, info_queue, center)
-    type_tooltip(self, info_queue, center)
-    if pokermon_config.detailed_tooltips then
-      info_queue[#info_queue+1] = G.P_CENTERS.m_gold
-    end
-    local card_vars = {center.ability.extra.Xmult}
-    add_target_cards_to_vars(card_vars, center.ability.extra.targets)
-    return {vars = card_vars}
-  end,
-  rarity = "poke_mega",
-  cost = 12,
-  stage = "Mega",
-  ptype = "Lightning",
-  atlas = "AtlasJokersBasicGen03",
-  gen = 3,
-  perishable_compat = true,
-  blueprint_compat = false,
-  eternal_compat = true,
-  calculate = function(self, card, context)
-    if context.individual and not context.end_of_round and context.cardarea == G.play and not context.other_card.debuff and SMODS.has_enhancement(context.other_card, 'm_gold') then
-      for i=1, #card.ability.extra.targets do
-        if context.other_card:get_id() == card.ability.extra.targets[i].id then
-            return {
-              Xmult = card.ability.extra.Xmult,
-              card = card
-            }
-        end
-      end
-    end
-  end,
-  set_ability = function(self, card, initial, delay_sprites)
-    if initial then
-      card.ability.extra.targets = get_poke_target_card_ranks("manectric", 3, card.ability.extra.targets)
-    end
-  end
 }
 
 return {
-  name = "Gem's Electrike",
-  enabled = Gem_config.Electrike or false,
-  list = { electrike, manectric, mega_manectric }
+  name = "Gem's Zeraora",
+  enabled = Gem_config.Zeraora or false,
+  list = { zeraora }
 }
 
 
