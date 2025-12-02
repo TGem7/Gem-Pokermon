36defbfa486711941f6e76ce0fcbc274e543c576
TGem7 <deeganmanzo1@gmail.com>
2025-12-02 17:34:07 -0500

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -68,9 +68,9 @@ return {
                 name = "Mega Sceptile",
                 text = {
                     "{C:attention}Nature: {C:inactive}({C:attention}#4#, #5#, #6#{C:inactive}){}",
-                    "Played {C:attention}Nature{} cards earn {C:money}$#1#{}, plus",
-                    "{C:money}$#1#{} for each other {X:grass,C:white}Grass{} or {X:dragon,C:white}Dragon{}",
-                    "card you have when scored",
+                    "Played {C:attention}Nature{} cards earn {C:money}$#1#{}",
+                    "when scored plus {C:money}$#1#{} for each other {X:grass,C:white}Grass{}",
+                    "or {X:dragon,C:white}Dragon{} card you have",
                     "{C:inactive,s:0.8}(includes Jokers and Energy cards){}",
                     "{C:inactive}(Currently {C:money}$#3#{C:inactive} total){}"
                 } 
