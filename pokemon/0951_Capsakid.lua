d3a70fcce4ca254ac075e4a74f2f815124a61220
TGem7 <deeganmanzo1@gmail.com>
2026-01-04 20:44:30 -0500

    Update 0951_Capsakid.lua

--- pokemon/0951_Capsakid.lua
+++ pokemon/0951_Capsakid.lua
@@ -12,15 +12,16 @@ local capsakid = {
   name = "capsakid",
   
   pos = PokemonSprites["capsakid"].base.pos,
-  config = {extra = {money = 1}},
+  config = {extra = {mult = 6}},
   loc_vars = function(self, info_queue, center)
     type_tooltip(self, info_queue, center)
+    info_queue[#info_queue+1] = G.P_CENTERS.m_poke_seed
     info_queue[#info_queue+1] = G.P_CENTERS.c_poke_firestone
-    return {vars = {center.ability.extra.money}}
+    return {vars = {center.ability.extra.mult}}
   end,
   rarity = 1, 
   cost = 4, 
-  enhancement_gate = 'm_mult',
+  enhancement_gate = 'm_poke_seed',
   item_req = "firestone",
   stage = "Basic", 
   ptype = "Grass",
@@ -28,12 +29,21 @@ local capsakid = {
   gen = 9,
   blueprint_compat = true,
   calculate = function(self, card, context)
-    if context.individual and not context.end_of_round and context.cardarea == G.play and not context.other_card.debuff and SMODS.has_enhancement(context.other_card,"m_mult") then
-      local earned = ease_poke_dollars(card, "capsakid", card.ability.extra.money, true)
+    if context.individual and not context.end_of_round and context.cardarea == G.play and not context.other_card.debuff and SMODS.has_enhancement(context.other_card,"m_poke_seed") then
       return {
-              dollars = earned,
-              card = card
+            message = localize('poke_spicy_ex'),
+            mult = card.ability.extra.mult,
+            card = card
       }
+    end
+    if context.individual and not context.end_of_round and context.cardarea == G.hand then
+      if SMODS.has_enhancement(context.other_card, 'm_poke_seed') then 
+          return {
+            message = localize('poke_spicy_ex'),
+            h_mult = card.ability.extra.mult,
+            card = card,
+          }
+      end
     end
     return item_evo(self, card, context, "j_Gem_scovillain")
   end
@@ -42,16 +52,15 @@ local capsakid = {
 -- Scovillain
 local scovillain = {
   name = "scovillain",
-  
   pos = PokemonSprites["scovillain"].base.pos,
-  config = {extra = {money = 3, mult = 4}},
+  config = {extra = {Xmult_multi = 2}},
   loc_vars = function(self, info_queue, center)
     type_tooltip(self, info_queue, center)
     if pokermon_config.detailed_tooltips then
       info_queue[#info_queue+1] = G.P_CENTERS.m_mult
-      info_queue[#info_queue+1] = G.P_CENTERS.m_gold
+      info_queue[#info_queue+1] = G.P_CENTERS.m_poke_seed
     end
-    return {vars = {center.ability.extra.money, center.ability.extra.mult}}
+    return {vars = {center.ability.extra.Xmult_multi}}
   end,
   rarity = "poke_safari", 
   cost = 8, 
@@ -61,31 +70,101 @@ local scovillain = {
   gen = 9,
   blueprint_compat = true,
   calculate = function(self, card, context)
-    if context.individual and not context.end_of_round and context.cardarea == G.play and not context.other_card.debuff and SMODS.has_enhancement(context.other_card,"m_mult") then
-      local earned = ease_poke_dollars(card, "scovillain", card.ability.extra.money, true)
-      return {
-              message = localize('poke_spicy_ex'),
-              dollars = earned,
-              card = card
-      }
+    if context.first_hand_drawn and not context.blueprint then
+      local eval = function() return G.GAME.current_round.hands_played == 0 and not G.RESET_JIGGLES end
+      juice_card_until(card, eval, true)
     end
-    if context.individual and not context.end_of_round and context.cardarea == G.hand then
-      if SMODS.has_enhancement(context.other_card, 'm_gold') then 
+      if context.individual and context.cardarea == G.play and SMODS.has_enhancement(context.other_card, 'm_mult') and G.GAME.current_round.hands_played == 0 and not context.blueprint then
+        local all_mult = true
+        for k, v in pairs(context.scoring_hand) do
+          if not SMODS.has_enhancement(v, "m_mult") then
+            all_mult = false
+          end
+        end
+        if all_mult then
+          local cards_held = {}
+            for k, v in ipairs(G.hand.cards) do
+              table.insert(cards_held, v)
+            end
+              pseudoshuffle(cards_held, pseudoseed('blacksludge'))
+              local limit = math.min(#cards_held, 1)
+              for i = 1, limit do
+                poke_convert_cards_to(cards_held[i], {mod_conv = 'm_poke_flower'}, true, true)
+                cards_held[i]:juice_up()
+              end
+        end
+      end
+        if context.individual and not context.end_of_round and context.cardarea == G.hand then
+          local suit_number = next(SMODS.find_card('j_poke_roserade')) and 3 or 4
+          if poke_suit_check(context.scoring_hand, suit_number) then
+      if SMODS.has_enhancement(context.other_card, 'm_poke_seed') then 
           return {
             message = localize('poke_spicy_ex'),
-            h_mult = card.ability.extra.mult,
+            xmult = card.ability.extra.Xmult_multi,
+            card = card,
+          }
+      elseif SMODS.has_enhancement(context.other_card, 'm_poke_flower') then 
+          return {
+            message = localize('poke_spicy_ex'),
+            xmult = card.ability.extra.Xmult_multi,
             card = card,
           }
       end
+            end
+        end
+  end,
+  megas = { "mega_scovillain" },
+}
+
+-- Mega Scovillain
+local mega_scovillain = {
+  name = "mega_scovillain",
+  pos = PokemonSprites["scovillain"].base.pos,
+  config = {extra = {Xmult_multi = 3}},
+  loc_vars = function(self, info_queue, center)
+    type_tooltip(self, info_queue, center)
+    if pokermon_config.detailed_tooltips then
+      info_queue[#info_queue+1] = G.P_CENTERS.m_poke_seed
     end
+    return {vars = {center.ability.extra.Xmult_multi}}
+  end,
+  rarity = "poke_mega", 
+  cost = 12, 
+  stage = "Mega", 
+  ptype = "Fire",
+  atlas = "AtlasJokersBasicNatdex",
+  gen = 9,
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+        if context.individual and not context.end_of_round and context.cardarea == G.hand then
+          local suit_number = next(SMODS.find_card('j_poke_roserade')) and 3 or 4
+          if poke_suit_check(context.scoring_hand, suit_number) then
+      if SMODS.has_enhancement(context.other_card, 'm_poke_seed') then 
+          return {
+            message = localize('poke_spicy_ex'),
+            xmult = card.ability.extra.Xmult_multi,
+            card = card,
+          }
+      elseif SMODS.has_enhancement(context.other_card, 'm_poke_flower') then 
+          return {
+            message = localize('poke_spicy_ex'),
+            xmult = card.ability.extra.Xmult_multi,
+            card = card,
+          }
+      end
+            end
+        end
   end
 }
 
 return {
   name = "Gem's Capsakid",
   enabled = Gem_config.Capsakid or false,
-  list = { capsakid, scovillain }
+  list = { capsakid, scovillain, mega_scovillain }
 }
+
+
+
 
 
 
