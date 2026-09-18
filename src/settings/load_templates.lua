84f12f226780026dba75da442dffcc1785e808b5
TGem7 <deeganmanzo1@gmail.com>
2026-09-17 23:43:07 -0400

    Crash fix

--- src/settings/load_templates.lua
+++ src/settings/load_templates.lua
@@ -1,20 +1,26 @@
 -- This is just the Load Pokemon file but tweaked a bit
-
 local poke_templates = {}
 
 local function load_template(item)
   local custom_prefix = item.Gem_inject_prefix or "Gem"
+
   local custom_atlas = item.atlas and string.find(item.atlas, "Gem")
 
   if not item.atlas then
-    poke_load_atlas(item)
-    poke_load_sprites(item)
+    pokermon.sprites.load_atlas(item)
+    pokermon.sprites.load_sprites(item)
   end
 
   item.atlas = (custom_atlas and "Gem_" or "poke_") .. item.atlas
   item.set = 'Joker'
   item.key = 'j_' .. custom_prefix .. '_' .. item.name
-  item.ability = item.config
+
+  poke_templates[item.key] = item
+end
+
+local function load_consumable_template(item)
+  item.atlas = "Gem_" .. item.atlas
+  item.key = 'c_Gem_' .. item.name
 
   poke_templates[item.key] = item
 end
@@ -31,7 +37,26 @@ local function load_pokemon_folder(folder)
 
       if poke.list and #poke.list > 0 then
         for _, item in ipairs(poke.list) do
-            load_template(item)
+          load_template(item)
+        end
+      end
+    end
+  end
+end
+
+local function load_consumables_folder(folder)
+  local files = NFS.getDirectoryItems(SMODS.current_mod.path .. folder)
+
+  for _, filename in ipairs(files) do
+    local file_path = SMODS.current_mod.path .. folder .. filename
+    local file_type = NFS.getInfo(file_path).type
+
+    if file_type ~= "directory" and file_type ~= "symlink" then
+      local cons = assert(SMODS.load_file(folder .. filename))()
+
+      if cons.list and #cons.list > 0 then
+        for _, item in ipairs(cons.list) do
+          load_consumable_template(item)
         end
       end
     end
@@ -39,5 +64,7 @@ local function load_pokemon_folder(folder)
 end
 
 load_pokemon_folder("pokemon/")
+
+load_consumables_folder("consumables/")
 
 return poke_templates
